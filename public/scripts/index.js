ce9b6497f95c209b1ca73237b755e589bdd5d271
Glitch (spiider34) <none>
2022-09-23 02:20:05 +0000

    💍🚋 Checkpoint ./public/styles/slider.css:49008067/163 ./src/pages/index.html:49008067/502 ./public/styles/main.css:49008067/49 ./public/scripts/index.js:49008067/1047

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,18 +1,30 @@
 const enter = 13;
 
+var qualitySetting = document.getElementById("qualitysetting")
+var typeSetting = document.getElementById("typesetting")
+var qualitySwitch = document.getElementById("quality")
+var typeSwitch = document.getElementById("type")
+var search = document.getElementById("search")
+
+var cite = "rule34";
+var quality = "full";
+
+
 addEvent(document, "keydown", function (e) {
   e = e || window.event;
 
   if (e.keyCode === enter) {
     var cite = "rule34";
-    if (document.getElementById("type").checked) {
+    var quality = "full"
+    
+    if (typeSwitch.checked) {
       cite = "hypnoh";
     }
-
+  
     if (document.activeElement.id === "search") {
       window.location.replace(
         `https://spiider34.glitch.me/p/${cite}?tags=${
-          document.getElementById("search").value
+          search.value
         }`
       );
     }
