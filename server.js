0376dea4bfd9050457620cde2184b4fe52f5c280
Glitch (spiider34) <none>
2022-10-23 03:20:37 +0000

    🚵🏃 Checkpoint ./src/pages/viewer.html:49008067/4041 ./server.js:49008067/27

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get("/autocomplete", function (req, res) {
   axios
     .get("https://" + req.query.sourse + "/public/autocomplete.php?q=" + req.query.q)
     .then(function (response) {
+      console.log()
       res.send(response.data);
     })
     .catch(function (error) {
