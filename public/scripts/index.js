17714710b2ec4c028b2504cd0b91ba24acc87ee3
Glitch (spiider34) <none>
2022-10-23 04:52:47 +0000

    🐬💶 Checkpoint ./public/scripts/index.js:49008067/2959

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,31 +1,48 @@
 const enter = 13;
 
-var qualitySetting = document.getElementById("qualitysetting")
-var typeSetting = document.getElementById("typesetting")
-var qualitySwitch = document.getElementById("quality")
-var typeSwitch = document.getElementById("type")
-var search = document.getElementById("search")
+var qualitySetting = document.getElementById("qualitysetting");
+var typeSetting = document.getElementById("typesetting");
+var qualityCombo = document.getElementById("quality");
+var typeCombo = document.getElementById("type");
+var search = document.getElementById("search");
 
-addEvent(typeSwitch, "change", updateCite)
-addEvent(qualitySwitch, "change", updateQuality)
+addEvent(typeSwitch, "change", updateCite);
+addEvent(qualitySwitch, "change", updateQuality);
 
-function updateCite(e) { 
-  if (e.srcElement.checked) { setSourse("hypnohub") } else { setSourse("Rule34") } 
-  typeSetting.innerHTML = 'Sourse: ' + data.sourse 
+function updateCite(e) {
+  if (e.srcElement.checked) {
+    setSourse("hypnohub");
+  } else {
+    setSourse("Rule34");
+  }
+  typeSetting.innerHTML = "Sourse: " + data.sourse;
 }
 
-function updateQuality(e) { 
-  if (e.srcElement.checked) { setQuality("Full") } else { setQuality("Sample") } 
-  qualitySetting.innerHTML = 'Quality: ' + data.quality 
+function updateQuality(e) {
+  if (e.srcElement.checked) {
+    setQuality("Full");
+  } else {
+    setQuality("Sample");
+  }
+  qualitySetting.innerHTML = "Quality: " + data.quality;
 }
 
-typeSetting.innerHTML = 'Sourse: ' + data.sourse 
-qualitySetting.innerHTML = 'Quality: ' + data.quality
+function findChild(element, child) {
+  let res = "";
+  for (var i = 0; i < element.children.length; i++) {
+    if (element.children[i].value === child) {
+      res = child;
+    }
+  }
+  return res;
+}
+
+typeSetting.innerHTML = "Sourse: " + data.sourse;
+qualitySetting.innerHTML = "Quality: " + data.quality;
 
 if (data.sourse === "hypnohub") {
-  typeSwitch.checked = true
+  typeSwitch.checked = true;
 }
 
-if (data.quality === "Full") {
-  qualitySwitch.checked = true
-}
+findChild(qualityCombo, data.quality).setAttribute(name, value)
+
