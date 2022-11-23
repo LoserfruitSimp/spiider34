c1e1e45f751fd471956e7c42bf506be1bc8e8579
Glitch (spiider34) <none>
2022-11-23 19:16:00 +0000

    🍐🚅 Checkpoint ./src/routing/posts.js:49008067/24641 ./package.json:49008067/962

--- src/routing/posts.js
+++ src/routing/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const postRouter = express.Router({ mergeParams: true });
 const host = "https://" + process.env.PROJECT_DOMAIN + ".glitch.me";
 const axios = require("axios").default;
+var parseString = require("xml2js").parseString;
 
 const urls = [
   "rule34.xxx",
@@ -10,6 +11,9 @@ const urls = [
   "realbooru.com",
   "xbooru.com",
 ];
+
+// List of urls that dont display the URL to file when using JSON request
+const XMLhash = ["safebooru.org", "realbooru.com", "xbooru.com"];
 
 postRouter.get("/", async function (req, res) {
   const cite = req.query.sourse;
@@ -22,8 +26,31 @@ postRouter.get("/", async function (req, res) {
       break;
     }
   }
-  
-  const api = await axios.get(
+
+  let data = [];
+
+  if (XMLhash.find((e) => e === baseURI)) {
+    // XML
+
+    const api = await axios.get(
+      "https://" +
+        baseURI +
+        "/index.php?page=dapi&s=post&q=index&tags=" +
+        req.query.tags +
+        "&pid=" +
+        pid / 100 +
+        "&json=0",
+      "text/xml"
+    );
+
+    parseString(api.data, function (err, result) {
+      for (let i = 0; i < result.posts.post.length; i++) result.posts.post[i] = result.posts.post[i].$
+      data = result.posts.post;
+    });
+  } else {
+    // JSON
+
+    const api = await axios.get(
       "https://" +
         baseURI +
         "/index.php?page=dapi&s=post&q=index&tags=" +
@@ -32,9 +59,12 @@ postRouter.get("/", async function (req, res) {
         pid / 100 +
         "&json=1",
       "application/json"
-    )
-  
-  res.json(api.data);
+    );
+
+    data = api.data;
+  }
+
+  res.json(data);
 });
 
 module.exports = postRouter;
