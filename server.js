f974fdcfaa8e0f2ea7bf74fb4279c856820755cd
Glitch (spiider34) <none>
2023-01-25 01:32:36 +0000

    👻🍡 Checkpoint ./server.js:49008067/1039 ./src/routing/autocomplete.js:49008067/10437

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const postRouter = require("./src/routing/posts");
 const fileRouter = require("./src/routing/files");
+const autocompleteRouter = require("./src/routing/autocomplete");
 
 const port = process.env.PORT;
 
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use("/posts", postRouter);
 app.use("/files", fileRouter);
+app.use("/autocomplete", autocompleteRouter);
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
@@ -20,10 +22,6 @@ app.get("/", function (req, res) {
 
 app.get("/p", function (req, res) {
   res.sendFile(path.join(__dirname, "/src/pages/viewer.html"));
-});
-
-app.get("/autocomplete", function (req, res) {
-  
 });
 
 app.listen(port, function () {
--- src/routing/autocomplete.js
+++ src/routing/autocomplete.js
@@ -3,16 +3,42 @@ const https = require("https");
 const fileRouter = express.Router();
 const axios = require("axios").default;
 
-fileRouter.get("/", function (req, res) {
+fileRouter.get("/", async function (req, res) {
   if (!req.query.url) res.sendStatus(404);
-
-  if (req.query.sourse === "gelbooru") {
-    
-  } else if (req.query.sourse === "hypnohub" || req.query.sourse === "rule34") {
-    axios
-      .get("https://" + req.query.sourse + "/public/autocomplete.php?q=" + req.query.q)
+  console.log(req.query.sourse)
+  if (req.query.sourse === "gelbooru.com") {
+    await axios
+      .get(
+        "https://" +
+          req.query.sourse +
+          "/index.php?page=autocomplete2&term=" +
+          req.query.q +
+          "&type=tag_query&limit=10"
+      )
       .then(function (response) {
-        res.send(response.data);
+        let data = [];
+        for (let i = 0; i < 10; i++) {
+          data.insert({
+            label:
+              response.data[i].label + " (" + response.data[i].post_count + ")",
+            value: response.data[i].value,
+          });
+        }
+        res.json(data);
+      })
+      .catch(function (error) {
+        res.send(error.message);
+      });
+  } else if (req.query.sourse === "hypnohub.net" || req.query.sourse === "rule34.xxx") {
+    axios
+      .get(
+        "https://" +
+          req.query.sourse +
+          "/public/autocomplete.php?q=" +
+          req.query.q
+      )
+      .then(function (response) {
+        res.json(response.data);
       })
       .catch(function (error) {
         res.send(error.message);
@@ -21,14 +47,12 @@ fileRouter.get("/", function (req, res) {
     axios
       .get("https://" + req.query.sourse + "/autocomplete.php?q=" + req.query.q)
       .then(function (response) {
-        res.send(response.data);
+        res.json(response.data);
       })
       .catch(function (error) {
         res.send(error.message);
       });
   }
-
-  res.json(data);
 });
 
 module.exports = fileRouter;
