60f5a1b202d18adaca83665eb388f1c8f000f4bb
Glitch (spiider34) <none>
2023-01-25 01:44:37 +0000

    🍣🍫 Checkpoint ./src/routing/autocomplete.js:49008067/3205

--- src/routing/autocomplete.js
+++ src/routing/autocomplete.js
@@ -1,35 +1,32 @@
 const express = require("express");
 const https = require("https");
-const fileRouter = express.Router();
+const fileRouter = express.Router({ mergeParams: true });
 const axios = require("axios").default;
 
 fileRouter.get("/", async function (req, res) {
-  if (!req.query.url) res.sendStatus(404);
-  console.log(req.query.sourse)
   if (req.query.sourse === "gelbooru.com") {
-    await axios
-      .get(
-        "https://" +
-          req.query.sourse +
-          "/index.php?page=autocomplete2&term=" +
-          req.query.q +
-          "&type=tag_query&limit=10"
-      )
-      .then(function (response) {
-        let data = [];
-        for (let i = 0; i < 10; i++) {
-          data.insert({
-            label:
-              response.data[i].label + " (" + response.data[i].post_count + ")",
-            value: response.data[i].value,
-          });
-        }
-        res.json(data);
-      })
-      .catch(function (error) {
-        res.send(error.message);
+    const response = await axios.get(
+      "https://" +
+        req.query.sourse +
+        "/index.php?page=autocomplete2&term=" +
+        req.query.q +
+        "&type=tag_query&limit=10"
+    );
+    
+    let data = [];
+    for (let i = 0; i < 10; i++) {
+      data.push({
+        label:
+          response.data[i].label + " (" + response.data[i].post_count + ")",
+        value: response.data[i].value,
       });
-  } else if (req.query.sourse === "hypnohub.net" || req.query.sourse === "rule34.xxx") {
+    }
+    
+    res.json(data);
+  } else if (
+    req.query.sourse === "hypnohub.net" ||
+    req.query.sourse === "rule34.xxx"
+  ) {
     axios
       .get(
         "https://" +
